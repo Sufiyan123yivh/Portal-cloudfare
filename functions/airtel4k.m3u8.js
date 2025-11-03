7d67ef764ad6347370a1e72eb7fd44d690413701
Sufiyan123yivh <farhanrehan2002@gmail.com>
2025-11-03 17:30:10 +0530

    Update airtel4k.m3u8.js

--- functions/airtel4k.m3u8.js
+++ functions/airtel4k.m3u8.js
@@ -86,7 +86,7 @@ export async function onRequest(context) {
   const id = urlObj.searchParams.get("id");
 
   try {
-    const baseUrl = `${urlObj.origin}/tatatv.m3u8`;
+    const baseUrl = `${urlObj.origin}/airtel4k.m3u8`;
 
     // Direct stream
     if (id) {
